c4ef9d5c558af936493af3c2f6bc0461e54ef825
Mel <dima.melov.03@mail.ru>
2022-07-27 14:40:06 +0300

    OOP

--- git-course.py
+++ git-course.py
@@ -1,2 +1,16 @@
-lst = [abs(float(x)) for x in input().split()]
-print(*lst)
+import re
+
+class CardCheck:
+    @staticmethod
+    def check_card_number(number):
+        if re.match(r"^\d{4}-\d{4}-\d{4}-\d{4}$",number):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_name(name):
+        if re.match(r'^[A-Z]+\s[A-Z]+$',name):
+            return True
+        else:
+            return False
